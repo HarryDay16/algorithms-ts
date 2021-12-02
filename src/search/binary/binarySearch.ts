31cf73e35ab1066394b39c9d1a17b7325464e5a4
Year 12 <year12@mar-admins-MacBook-Air-3.local>
2021-12-02 09:12:39 +0000

    binarySearch

--- src/search/binary/binarySearch.ts
+++ src/search/binary/binarySearch.ts
@@ -1,8 +1,38 @@
 import { NO_MATCH } from "../common"
 
-const binarySearch = (items: any[], item: any): number => {
+const binarySearch = (items: any[], item: any) => {
+    let result = null;
 
-    return NO_MATCH;
+    let lowestPoint = 0
+    let highestPoint = items.length - 1
+    let middlePoint; 
+    while(result == null){
+        middlePoint = Math.floor((highestPoint + lowestPoint) / 2)
+        
+        if (item == items[middlePoint]){
+            result = middlePoint
+        }
+        else if(lowestPoint == highestPoint){
+            result = NO_MATCH
+        }
+        else if(item > items[middlePoint]){
+            if (middlePoint == 0){
+                result = NO_MATCH
+            }
+            lowestPoint = middlePoint + 1
+           
+        }
+        else if(item < items[middlePoint]){
+            if (middlePoint == items.length - 1){
+                result = NO_MATCH
+            }
+             highestPoint = middlePoint - 1
+        }
+
+    }
+    return result;
 }
+ 
+
 
 export default binarySearch;
--- src/search/linear/linearSearch.ts
+++ src/search/linear/linearSearch.ts
@@ -2,12 +2,13 @@ import { NO_MATCH } from "../common";
 
 
 const linearSearch = (items: any[], item: any): number => {
-for(let counter:number = 0; counter <= items.length; counter++ ){
-if (items[counter] == item){
-    return counter
+    for(let counter:number = 0; counter <= items.length; counter++ ){
+        if (items[counter] == item){
+            return counter
+        }
+    }
+    return NO_MATCH
 }
-}
-return NO_MATCH}
 
 
 export default linearSearch;
